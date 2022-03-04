300b12adcf22f5957a2f102b2c96ac190f636674
jharshitgoogle <99312335+jharshitgoogle@users.noreply.github.com>
2022-03-04 10:33:02 +0530

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -1,9 +1,3 @@
-echo "Hello fro installer script"
+echo "Hello from installer script"
 $bootstrapToken=$1
 echo $bootstrapToken
-
-# Step2: install osconfig
-sudo sh -c "echo 'deb http://packages.cloud.google.com/apt google-osconfig-agent-stable main' >> /etc/apt/sources.list"
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-sudo apt-get update
-sudo apt-get install -y google-osconfig-agent
