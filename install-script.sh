9d02276c7e56e0534f88288b1de4229874072ca5
jharshitgoogle <99312335+jharshitgoogle@users.noreply.github.com>
2022-03-04 10:54:18 +0530

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -2,4 +2,4 @@ echo "Hello from installer script"
 bootstrapToken=$1
 echo $bootstrapToken
 
-curl -X POST https://httpbin.org/post -H "Content-Type: application/json" -d '{"bootstrapToken" : $bootstrapToken}'
+curl -X POST https://httpbin.org/post -H "Content-Type: application/json" -d '{"bootstrapToken" : "'"$bootstrapToken"'"}'
