afc11512204a5dd75515ebe02a7b4e24e278abc0
jharshitgoogle <99312335+jharshitgoogle@users.noreply.github.com>
2022-03-16 07:24:38 +0000

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -1,13 +1,14 @@
-
 bootstraptoken=$1
 vmID=$2
 # todo:jharshit: in next PR: how to get this fix ip?
 attachServiceEp="10.110.5.236"
 
 echo "Calling attach service to configure workload identity"
-configureWIURL="$attachServiceEp""/installagent0/configureworkloadidentityforvm/"
-HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST configureWIURL -H "Content-Type: application/json" -d '{"BootstrapToken" : "'"$bootstrapToken"'", "VmId" : "'"$vmID"'"}')
+configureWIURL="http://""$attachServiceEp""/installagent0/configureworkloadidentityforvm/"
+echo "Hitting "$configureWIURL
+HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$configureWIURL" -H "Content-Type: application/json" -d '{"BootstrapToken" : "'"$bootstrapToken"'", "VmId" : "'"$vmID"'"}')
 # todo:jharshit: in next PR: get 4 things and dump into file
+echo "configure workload identity status: "$HTTP_STATUS
 
 if [ $HTTP_STATUS != 200 ]
 then
@@ -15,18 +16,19 @@ then
 else
   echo "Configure Workload identity successful , Installing osconfig agent"
   sudo sh -c "echo 'deb http://packages.cloud.google.com/apt google-osconfig-agent-stable main' >> /etc/apt/sources.list"
-  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-  sudo apt-get update
-  sudo apt-get install -y google-osconfig-agent
+  # curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  # sudo apt-get update
+  # sudo apt-get install -y google-osconfig-agent
 
   echo "Starting up osconfig agent"
   sh ./osconfig
 
   echo "Getting agent running status"
   agent_status=$(systemctl status google-osconfig-agent 2>&1 > /dev/null | grep running)
+  agentStatus=false
   if test -z $agent_status; then agentStatus=false && echo "Agent not Running"; else agentStatus=true && echo "Agent Running"; fi
 
-  echo "Updating agent status to attach-service"
-  UpdateInstallStatusURL="$attachServiceEp""/installagent0/updateinstallstatus/"
-  curl -X POST UpdateInstallStatusURL -H "Content-Type: application/json" -d '{"AgentRunning" : $agentStatus}'
+  echo "Updating agent status to attach-service with status "$agentStatus
+  UpdateInstallStatusURL="http://""$attachServiceEp""/installagent0/updateinstallstatus/"
+  curl -X POST "$UpdateInstallStatusURL" -H "Content-Type: application/json" -d '{"AgentRunning" : '"$agentStatus"'}'
 fi
